d8353c6d51d5dc301b7ec0155e981f12e7829e3b
Lama Ibrahim <lamai@amdocs.com>
2023-02-09 09:23:33 +0200

    fix profileController name

--- src/routes/profile.routers.js
+++ src/routes/profile.routers.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
-const jobControllers = require('../controllers/profile.controllers');
+const profileControllers = require('../controllers/profile.controllers');
 const { getProfile } = require('../middleware/getProfile');
 
 const router = express.Router();
 
-router.post('/balances/deposit/:userId', getProfile, jobControllers.depositeBalances)
+router.post('/balances/deposit/:userId', getProfile, profileControllers.depositeBalances)
 
-router.get('/admin/best-profession', getProfile, jobControllers.getBestProfession)
+router.get('/admin/best-profession', getProfile, profileControllers.getBestProfession)
 
-router.get('/admin/best-clients', getProfile, jobControllers.getBestClients)
+router.get('/admin/best-clients', getProfile, profileControllers.getBestClients)
 
 module.exports = router;
